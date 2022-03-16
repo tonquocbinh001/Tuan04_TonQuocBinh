da01e2541633f3b4f8310adc934a64ca4815d70e
Thien Quoc <talavipcute5555@gmail.com>
2022-03-16 13:55:25 +0700

    first commit

---
+++ Tuan04_TonQuocBinh/Models/GioHang.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tuan04_TonQuocBinh.Models
+{
+    public class GioHang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int masach { get; set; }
+
+        [Display(Name = "Tên sách")]
+        public string tensach { get; set; }
+
+        [Display(Name = "Ảnh bìa")]
+        public string hinh { get; set; }
+
+        [Display(Name = "Giá bán")]
+        public Double giaban { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get { return iSoluong * giaban; }
+        }
+
+        public GioHang(int id)
+        {
+            masach = id;
+            Sach sach = data.Saches.Single(n => n.masach == masach);
+            tensach = sach.tensach;
+            hinh = sach.hinh;
+            giaban = double.Parse(sach.giaban.ToString());
+            iSoluong = 1;
+        }
+    }
+}
